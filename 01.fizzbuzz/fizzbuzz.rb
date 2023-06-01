84d06541347ce29631ed8c1114d2e6ff79b65701
Hombo-Yasutaka <y.honbo@genbasupport.com>
2023-06-02 08:10:44 +0900

    second fizzbuzz commit

--- 01.fizzbuzz/fizzbuzz.rb
+++ 01.fizzbuzz/fizzbuzz.rb
@@ -1,10 +1,9 @@
 (1..20).each do |num|
-  case
-  when num % 15 == 0
+  if num % 15 == 0
     puts "FizzBuzz"
-  when num % 5  == 0
+  elsif num % 5  == 0
     puts "Buzz"
-  when num % 3  == 0
+  elsif num % 3  == 0
     puts "Fizz"
   else
     puts num 
