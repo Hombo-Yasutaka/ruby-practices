821866e8b7c1731a858a4794c0f6801ae00fd6ee
Hombo-Yasutaka <y.honbo@genbasupport.com>
2023-06-12 14:21:02 +0900

    メソッド分割の修正

--- 07.ls_object/ls.rb
+++ 07.ls_object/ls.rb
@@ -2,39 +2,34 @@
 
 COLUMN = 3.0
 
-# 返り値はCOLUMN × output_rowの行列(2次元配列)を想定
-def generate_files_2d(files, output_row)
-  files_2d = Array.new(COLUMN) { Array.new(output_row) { '' } }
+# 返り値はCOLUMN × row_sizeの行列(2次元配列)を想定
+def generate_file_list(files)
+  # 出力する際の行数
+  row_size = (files.size / COLUMN).ceil
+  file_list = Array.new(COLUMN) { Array.new(row_size) { '' } }
   files.each_with_index do |file, index|
-    column = index / output_row
-    row = index % output_row
-    files_2d[column][row] = file
+    column = index / row_size
+    row = index % row_size
+    file_list[column][row] = file
   end
-  files_2d
+  file_list
 end
 
-# 引数filesは、['file1', 'file2', ...]という形を想定
-def calculate_max_file_name_length(files)
-  files.max_by(&:length).length
-end
-
-def align_file_name_length_with_spaces(files, length)
-  files.map { |file| file.ljust(length, ' ') }
+def print_file_list(file_list)
+  file_list_for_output = []
+  file_list.each do |files|
+    max_length = files.max_by(&:length).length
+    file_list_for_output << files.map { |file| file.ljust(max_length, ' ') }
+  end
+  # file_list_for_output(COLUMN × row_size行列)をCOLUMN列で出力する
+  file_list_for_output.transpose.each do |files|
+    puts files.join('  ')
+  end
 end
 
 input_files = Dir.glob('*')
 
 if !input_files.empty?
-  # 出力する際の行数
-  output_row = (input_files.size / COLUMN).ceil
-  files_2d = generate_files_2d(input_files, output_row)
-  aligned_files_2d = []
-  files_2d.each do |files_1d|
-    max_length = calculate_max_file_name_length(files_1d)
-    aligned_files_2d << align_file_name_length_with_spaces(files_1d, max_length)
-  end
-  # aligned_files_2d(COLUMN × output_row行列)をCOLUMN列で出力する
-  aligned_files_2d.transpose.each do |files_1d|
-    puts files_1d.join('  ')
-  end
+  file_list = generate_file_list(input_files)
+  print_file_list(file_list)
 end
